030587a8174fcd083b4ea77424d977c5bde2ab3d
uspl-0085 <uspl-0085@USPL-085.unicodesystems.ad>
2020-08-08 17:22:43 +0530

    adding testing class to check build

---
+++ APIAutomation/src/test/java/org/testing/TestScripts/TestingBuild.java
@@ -0,0 +1,12 @@
+package org.testing.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class TestingBuild {
+
+	@Test
+	public void TC1()
+	{
+		System.out.println("Build running sucessfully");
+	}
+}
